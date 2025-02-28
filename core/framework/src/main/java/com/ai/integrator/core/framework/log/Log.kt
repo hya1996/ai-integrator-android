1c87399b3152e1576d23c6fc8b159f5a8b7e2cd9
SpringBird <hyaa1996@gmail.com>
2025-02-28 16:50:01 +0800

    feat: 添加Log类

---
+++ core/framework/src/main/java/com/ai/integrator/core/framework/log/Log.kt
@@ -0,0 +1,25 @@
+package com.ai.integrator.core.framework.log
+
+import android.util.Log
+
+object Log {
+    fun v(tag: String, message: String) {
+        Log.v(tag, message)
+    }
+
+    fun d(tag: String, message: String) {
+        Log.d(tag, message)
+    }
+
+    fun i(tag: String, message: String) {
+        Log.i(tag, message)
+    }
+
+    fun w(tag: String, message: String) {
+        Log.w(tag, message)
+    }
+
+    fun e(tag: String, message: String) {
+        Log.e(tag, message)
+    }
+}
